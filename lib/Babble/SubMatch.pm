d453c5f38d888a25f0896aedcc830651d632dd1a
Matt S Trout <mst@shadowcat.co.uk>
2018-07-07 21:41:21 +0000

    add transform_text method for sugar

--- lib/Babble/SubMatch.pm
+++ lib/Babble/SubMatch.pm
@@ -12,6 +12,14 @@ sub replace_text {
   $self->replace_substring(0, length($self->text), $new_text);
 }
 
+sub transform_text {
+  my ($self, $code) = @_;
+  local $_ = my $text = $self->text;
+  $code->($_);
+  if ($_ ne $text) { $self->replace_text($_) }
+  return $self;
+}
+
 before replace_substring => sub {
   my ($self, $start, $length, $new_text) = @_;
   $self->parent
