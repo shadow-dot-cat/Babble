209dc20e677eeef36c6157781ecd5c883726b8a1
Matt S Trout <mst@shadowcat.co.uk>
2018-08-16 18:10:40 +0000

    add PluginChain and Filter sketches

---
+++ lib/Babble/Filter.pm
@@ -0,0 +1,19 @@
+package Babble::Filter;
+
+use Babble::PluginChain;
+use Filter::Util::Call;
+use strictures 2;
+
+sub import {
+  my ($class, @plugins) = @_;
+  my $pc = Babble::PluginChain->new;
+  $pc->add_plugin($_) for @plugins;
+  filter_add(sub {
+    filter_del();
+    1 while filter_read();
+    $_ = $pc->transform_document($_);
+    return 1;
+  });
+}
+
+1;
