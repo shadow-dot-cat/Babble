66c047146bd4f8066d5e4acb3ea3a4b06f1a8e5e
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-08-11 10:58:45 -0400

    Add parentheses around assignment
    Assignment (=) is lower precedence than ternary (?:).

--- lib/Babble/Plugin/DefinedOr.pm
+++ lib/Babble/Plugin/DefinedOr.pm
@@ -29,7 +29,7 @@ sub transform_to_plain {
     my ($before, $after) = $m->subtexts(qw(before after));
     s/^\s+//, s/\s+$// for ($before, $after);
     if ($m->submatches->{op}->text =~ /=$/) {
-      $after = '$_ = '.$after;
+      $after = '($_ = '.$after.')';
     }
     $m->replace_text('(map +(defined($_) ? $_ : '.$after.'), '.$before.')[0]');
   };
